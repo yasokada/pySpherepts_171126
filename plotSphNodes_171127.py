0d30de7dab0dbc7ab268bcaeccc89846b9fa7424
yasokada <yasokada@gmail.com>
2017-12-02 11:14:04 +0900

    plotSphNodes_171127: add extractFrontPoints()

--- plotSphNodes_171127.py
+++ plotSphNodes_171127.py
@@ -2,8 +2,11 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 from pylab import rcParams
+import sys
 
 '''
+v0.3 Dec. 03, 2017
+    - add extractFrontPoints()
 v0.2 Dec. 02, 2017
     - plotSphNodes() takes [elevation] and [azimuth] args
 v0.1 Nov. 27, 2017
@@ -11,6 +14,43 @@ v0.1 Nov. 27, 2017
 '''
 
 # on Matplotlib
+
+def extractFrontPoints(data, elevation, azimuth):
+    xx, yy, zz = np.hsplit(data, 3)
+
+    # 1. prepare the rotation matrix
+    theta = np.radians(elevation)
+    phi = np.radians(azimuth)
+    # 1-1. rotation for theta around the y axis
+    rottht = [
+        [np.cos(theta), 0.0, np.sin(theta)],
+        [0.0, 1.0, 0.0],
+        [-np.sin(theta), 0.0, np.cos(theta)],
+    ]
+    # 1-2. rotation for phi around the z axis
+    rotphi = [
+        [np.cos(-phi), -np.sin(-phi), 0.0],
+        [np.sin(-phi), np.cos(-phi), 0.0],
+        [0.0, 0.0, 1.0],
+    ]
+
+    # 2. Plots in the hidden side are removed
+    #   since the code will display hidden plots
+    xs, ys, zs = [], [], []
+    for axo, ayo, azo in zip(xx, yy, zz):
+        # 2-1. rotatio in theta and phi
+        #   **rotate in theta firstly will fail**
+        wrk = np.matmul(rotphi, [axo, ayo, azo])
+        axr, ayr, azr = np.matmul(rottht, wrk)
+
+        # 2-2. extract only front points
+        if (axr > 0):
+            xs += [axo]
+            ys += [ayo]
+            zs += [azo]
+    xx, yy, zz = xs, ys, zs
+
+    return xx, yy, zz
 
 
 def plotSphNodes(data, elevation=0.0, azimuth=0.0):
@@ -25,17 +65,7 @@ def plotSphNodes(data, elevation=0.0, azimuth=0.0):
     z = r*cos(phi)
 
     data = data * 1.0005
-    xx, yy, zz = np.hsplit(data, 3)
-
-    # Plots in the hidden side are removed
-    # since the code will display hidden plots
-    xs, ys, zs = [], [], []
-    for ax, ay, az in zip(xx, yy, zz):
-        if (ax > 0):
-            xs += [ax]
-            ys += [ay]
-            zs += [az]
-    xx, yy, zz = xs, ys, zs
+    xx, yy, zz = extractFrontPoints(data, elevation, azimuth)
 
     # Set colours and render
     fig = plt.figure()
